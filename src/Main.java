72a97185f55d23635bc7915543a7fd74c01738ef
Igor <igor1984@yandex.ru>
2020-07-25 14:45:43 +0300

    human readable & readFromHumanReadable methods

--- src/Main.java
+++ src/Main.java
@@ -43,14 +43,32 @@ public class Main {
         }
         String sizeUnit = null;
         for (int i = 0; folderSize / Math.pow(BYTE_FACTOR, i) > 1; i++) {
-            if (i == 0) sizeUnit = "B";
+            if (i == 0) sizeUnit = "байт";
             else if (i == 1) sizeUnit = "Кб";
             else if (i == 2) sizeUnit = "Мб";
-            else if (i >= 3) {
-                sizeUnit = "Гб";
-                break;
-            }
+            else if (i == 3) {
+                sizeUnit = "Гб"; }
+            else sizeUnit = "Тб";
+            break;
         }
         return String.format("%s%s", size, sizeUnit);
+    }
+
+    private static long getFromHumanReadableSize(String folderString) {
+        final int BYTE_FACTOR = 1024;
+        long size = Long.valueOf(folderString.replaceAll("[^0-9]",""));
+        if (folderString.contains("байт")){
+            return size;
+        }
+        else if (folderString.contains("Кб")){
+            return size * BYTE_FACTOR;
+        }
+        else if (folderString.contains("Мб")){
+            return (long) (size * Math.pow(BYTE_FACTOR,2));
+        }
+        else if (folderString.contains("Гб")){
+            return (long) (size * Math.pow(BYTE_FACTOR, 3));
+        }
+        else return (long) (size * Math.pow(BYTE_FACTOR, 4));
     }
 }
