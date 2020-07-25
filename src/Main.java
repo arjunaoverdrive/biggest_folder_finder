50912a5770916ed0ada002be2d0f1199ed4ae469
Igor <igor1984@yandex.ru>
2020-07-25 14:34:36 +0300

    human readable

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import java.io.File;
-import java.io.IOException;
+import java.util.concurrent.ForkJoinPool;
 import java.util.stream.Stream;
 
 public class Main {
@@ -7,7 +7,18 @@ public class Main {
         try {
             String folderPath = "C:\\Users\\Igor\\Desktop\\Magazines";
             File file = new File(folderPath);
-            System.out.println(getFolderSize(file));
+            long start = System.currentTimeMillis();
+
+            FolderSizeCalculator calculator =
+                    new FolderSizeCalculator(file);
+            ForkJoinPool pool = new ForkJoinPool();
+            long size = pool.invoke(calculator);
+            System.out.println(getHumanReadableSize(size));
+
+            //System.out.println(getFolderSize(file));
+
+            long duration = System.currentTimeMillis() - start;
+            System.out.println(duration + " ms");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -21,5 +32,25 @@ public class Main {
         long sum = Stream.of(files).mapToLong(File::length).sum();
 
         return sum;
+    }
+
+    public static String getHumanReadableSize(float folderSize){
+        final int BYTE_FACTOR = 1024;
+        float size = 0;
+        for (int i = 0; folderSize / Math.pow(BYTE_FACTOR, i) >= 1; i++) {
+            size = Math.round((float) (folderSize / Math.pow(BYTE_FACTOR, i)));
+            if (i > 3) break;
+        }
+        String sizeUnit = null;
+        for (int i = 0; folderSize / Math.pow(BYTE_FACTOR, i) > 1; i++) {
+            if (i == 0) sizeUnit = "B";
+            else if (i == 1) sizeUnit = "Кб";
+            else if (i == 2) sizeUnit = "Мб";
+            else if (i >= 3) {
+                sizeUnit = "Гб";
+                break;
+            }
+        }
+        return String.format("%s%s", size, sizeUnit);
     }
 }
